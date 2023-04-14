8f73c4e7ae33219420c24de20a740faa665a2f39
tonejman <nej.man@interia.pl>
2023-04-14 20:40:00 +0200

    task completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const newEl = document.createElement("li");
+  newEl.textContent = ingredient;
+  newEl.classList.add("item");
+  ingredientsList.append(newEl);
+});
