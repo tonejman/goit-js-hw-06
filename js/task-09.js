1360d5146216ceed00e355aa09d8aa884995b5d1
tonejman <nej.man@interia.pl>
2023-04-17 13:55:24 +0200

    task completed

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const spanTextColor = document.querySelector(".color");
+
+buttonChangeColor.addEventListener("click", () => {
+  const valueColor = getRandomHexColor();
+  body.style.backgroundColor = valueColor;
+  spanTextColor.textContent = valueColor;
+});
